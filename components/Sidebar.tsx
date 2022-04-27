5d407fd76586d54d94bc08fed4544e24460f2f81
aviaryan77 <aviaryan77@gmail.com>
2022-04-28 02:29:15 +0530

    updated contact details

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 import { AiFillGithub } from '@react-icons/all-files/ai/AiFillGithub';
 import { AiFillLinkedin } from '@react-icons/all-files/ai/AiFillLinkedin';
-import { AiFillYoutube } from '@react-icons/all-files/ai/AiFillYoutube';
+import { IoLogoWhatsapp } from '@react-icons/all-files/io/IoLogoWhatsapp';
 import { GoLocation } from '@react-icons/all-files/go/GoLocation';
 import { GiTie } from '@react-icons/all-files/gi/GiTie';
 import { useTheme } from 'next-themes';
@@ -14,11 +12,6 @@ const Sidebar = () => {
   const changeTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
   return (
     <div>
-      {/* <img
-        src="/images/profile.png"
-        alt="profile avatar"
-        className="w-32 h-32 mx-auto border rounded-full"
-      /> */}
       <div className="items-center justify-center w-32 h-32 mx-auto border rounded-full ">
         <Image
           src="/images/profile.png"
@@ -37,21 +30,21 @@ const Sidebar = () => {
         React, React Native Developer
       </p>
       <a
-        href="/assets/Avinash Aryan Resume.pdf"
-        download="Avinash Aryan Resume.pdf"
+        href="/CV_Avinash.pdf"
+        download="CV_Avinash.pdf"
         className="flex items-center justify-center px-2 py-1 my-2 bg-gray-200 rounded-full dark:bg-dark-200"
       >
         <GiTie className="w-6 h-6" /> <span>Download Resume</span>
       </a>
       {/* social icons */}
       <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full">
-        <a href="">
-          <AiFillYoutube className="w-8 h-8 cursor-pointer" />
+        <a href="https://wa.me/919431210691">
+          <IoLogoWhatsapp className="w-8 h-8 cursor-pointer" />
         </a>
-        <a href="">
+        <a href="https://github.com/aviaryan77">
           <AiFillGithub className="w-8 h-8 cursor-pointer" />
         </a>
-        <a href="">
+        <a href="https://www.linkedin.com/in/avinash-aryan/">
           <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
         </a>
       </div>
@@ -61,11 +54,21 @@ const Sidebar = () => {
           <GoLocation />
           <span>Patna, India</span>
         </div>
-        <p className="my-2 ">aviaryan77@gmail.com</p>
-        <p className="my-2">9431210691</p>
+        <p
+          onClick={() => window.open('mailto:aviaryan77@gmail.com')}
+          className="my-2 "
+        >
+          aviaryan77@gmail.com
+        </p>
+        <p onClick={() => window.open('tel:+919431210691')} className="my-2">
+          +91 9431210691
+        </p>
       </div>
       <div>
-        <button className="w-8/12 px-5 py-2 my-3 rounded-full bg-gradient-to-r from-green-400 to-blue-400">
+        <button
+          onClick={() => window.open('mailto:aviaryan77@gmail.com')}
+          className="w-8/12 px-5 py-2 my-3 rounded-full bg-gradient-to-r from-green-400 to-blue-400"
+        >
           Email Me
         </button>
         <button
