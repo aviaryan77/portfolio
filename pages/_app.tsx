2fb6c8d4e1f9f28e6039775079557bc8a93edbb0
aviaryan77 <aviaryan77@gmail.com>
2023-07-13 16:19:54 +0530

    added head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,22 @@ import '../styles/globals.css';
 import { Navbar, Sidebar } from '@/components';
 import { ThemeProvider } from 'next-themes';
 import { AnimatePresence } from 'framer-motion';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="title" content={'Avinash Aryan'}  />
+        <meta
+          name="keywords"
+          content="Avi aryan, Frontend developer, mobile app developer in patna"
+        />
+        Accept-Language: fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5
+      </Head>
       <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-12 xl:px-20 2xl:px-48 ">
         <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-dark-500 lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark">
           <Sidebar />
